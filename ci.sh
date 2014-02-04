b893845550c0c0186bbd612f06e5065b9d0839f1
Nan Monnand Deng <monnand@gmail.com>
2014-02-04 14:02:00 -0500

    csindent

--- ci.sh
+++ ci.sh
@@ -2,5 +2,8 @@
 PWD=`git rev-parse --show-toplevel`
 cp ~/.vimrc ${PWD}/vimrc
 mkdir -p ${PWD}/.vim/plugin
-cp ~/.vim/plugin/csindent.vim ${PWD}/.vim/plugin
+mkdir -p ${PWD}/.vim/csindent/cpp
+cp ~/.vim/plugin/csindent.vim ${PWD}/.vim/plugin/csindent.vim
+cp ~/.vim/csindent/cpp/google.vim ${PWD}/.vim/csindent/cpp/google.vim
+cp ~/.vim_csindent.ini ${PWD}/.vim_csindent.ini
 git commit -a
